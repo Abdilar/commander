d921502ee7d188047627a3f14c85b1e31df7d1a3
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2024-04-03 19:51:00 +0330

    added an option with default value

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ program
   .option('-e, --error', 'showing error message')
   .option('-v, --version <version>', 'version of file')
   .option('-fn, --file-name <file-name>', 'the file name')
+  .option('-p | --port <number>', 'port number', '8080')
 
 program.parse(process.argv);
 
@@ -16,3 +17,5 @@ if (options.error) {
 if (options.version) {
   console.log('the version is ', options.version);
 }
+
+console.log('The port is ', options.port);
