82ecf02a94231fb5cc7dff4d4dd4f3855d59268d
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2024-04-10 18:16:04 +0330

    prettify command file

--- command.js
+++ command.js
@@ -1,24 +1,21 @@
-const commander = require('commander')
-const path = require('path')
-const fs = require('fs')
+const commander = require("commander");
+const path = require("path");
+const fs = require("fs");
 
-const program = new commander.Command()
+const program = new commander.Command();
+
+program.command("tea").action(() => {
+  console.log("tea is ready....");
+});
 
 program
-  .command('tea')
-  .action(() => {
-    console.log('tea is ready....')
-  })
+  .command("create <fileName> [destination]")
+  .description("create a new file with the specified file name")
+  .alias("cr")
+  .action((fileName, destination = ".") => {
+    const dir = path.join(__dirname, destination, fileName);
+    fs.writeFileSync(dir, "");
+    console.log("created a file...");
+  });
 
-
-  program
-    .command('create <fileName> [destination]')
-    .description('create a new file with the specified file name')
-    .alias('cr')
-    .action((fileName, destination = '.') => {
-      const dir = path.join(__dirname, destination, fileName)
-      fs.writeFileSync(dir, '')
-      console.log('created a file...');
-    })
-
-  program.parse()
+program.parse();
