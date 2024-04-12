72a6e304d70de4723d5f473d5a2e2d1fff9063d9
Saeed Abdilar <40013688+Abdilar@users.noreply.github.com>
2024-04-12 18:20:02 +0330

    add stand-alone executable command

--- action.js
+++ action.js
@@ -6,18 +6,34 @@ program
   .option("-d, --debug", "Print debug information")
   .action((name, options, command) => {
     if (options.debug) {
-      console.log('Called %s with options %o', command.name(), options);
+      console.log("Called %s with options %o", command.name(), options);
     }
-    const title = options.title ? `${options.title} ` : '';
+    const title = options.title ? `${options.title} ` : "";
     console.log(`Thank-you ${title}${name}`);
   });
 
 program
-  .command('server')
-  .argument('<script>')
-  .option('-p, --port <port>', 'the port of server')
+  .command("server")
+  .argument("<script>")
+  .option("-p, --port <port>", "the port of server")
   .action(function () {
-    console.error('Run script %s on port %s', this.args[0], this.opts().port);
+    console.error("Run script %s on port %s", this.args[0], this.opts().port);
+  });
+
+program
+  .name("sakit")
+  .version("1.0.0")
+  .description("The SAkit css package")
+  .command("install [name]", "install one or more packages")
+  .alias("i")
+  .command("search [query]", "search with optional query")
+  .alias("s")
+  .command("update", "update installed packages", {
+    executableFile: "myUpdateSubCommand",
   })
+  .alias("u")
+  .action((name, options, command) => {
+    console.log({ name, options, command });
+  });
 
 program.parse();
